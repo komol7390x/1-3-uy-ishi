5a74ebcf2ebd9adcd53e0db845cd6d48a9e8ac7b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-09 18:58:14 +0500

    u

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// 1-vazifa
+// const first=parseInt(prompt("1-soni kiriting: "))
+// const second=parseInt(prompt("2-soni kiriting: "))
+// alert(first+second)
+
+// 2-vazifa
+// const firstName=prompt("Ismni kiriting: ")
+// const lastName=prompt("Familyani kiriting: ")
+// alert(firstName+ " " +lastName)
+
+// 3-vazifa
+// const age=parseInt(prompt("Yoshingizni kiriting"))
+// if (age>=18){
+//     alert("Siz voyaga yetgansiz.")
+// }else alert("Siz voyaga yetmagansiz")
+
+// 4-vazifa
+// const number=parseInt(prompt("Son kiriting:"))
+// if (number%2==0){
+//     alert("Kiritilgan son juft.")
+// }else alert('Kiritilgan son toq."')
+
+// 5-vazifa
+// const score=parseInt(prompt("Ballni kiriting:"))
+// if (score>=90 && score<=100){
+//     alert("A")
+// }else if (score>=80 && score<90){
+//     alert("B")
+// }else if (score>=70 && score<80){
+//     alert("C")
+// }else if (score>=60 && score<70){
+//     alert("D")
+// }else alert("F")
+
+// 6-vazifa
+
